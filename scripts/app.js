b165bf34b3bffbe57fbbc39d80d7febb350afe0d
Paul Lewis <github@aerotwist.com>
2015-03-31 21:57:07 +0100

    First commit.

---
+++ scripts/app.js
@@ -0,0 +1,148 @@
+APP.Main = (function() {
+
+  var stories = null;
+  var storyStart = 0;
+  var count = 100;
+  var $ = document.querySelector.bind(document);
+  var main = $('main');
+
+  var storyTemplate =
+      Handlebars.compile($('#tmpl-story').textContent);
+  var storyDetailsTemplate =
+      Handlebars.compile($('#tmpl-story-details').textContent);
+
+  function createStoryListItem (id) {
+    var story = document.createElement('div');
+    story.setAttribute('id', 's-' + id);
+    story.classList.add('story');
+    main.appendChild(story);
+  }
+
+  /**
+   * As every single story arrives in shove its
+   * content in at that exact moment. Feels like something
+   * that should really be in a requestAnimationFrame.
+   */
+  function onStoryData (key, details) {
+    var story = document.querySelector('#s-' + key);
+    var html = storyTemplate(details);
+    story.innerHTML = html;
+    story.addEventListener('click', onStoryClick.bind(this, details));
+  }
+
+  function onStoryClick(details) {
+
+    var storyDetails = $('sd-' + details.id);
+
+    // Create and append the story. A visual change...
+    // perhaps that should be in a requestAnimationFrame?
+    // And maybe, since they're all the same, I don't
+    // need to make a new element every single time? I mean,
+    // it inflates the DOM and I can only see one at once.
+    if (!storyDetails) {
+
+      var html = storyDetailsTemplate(details);
+      storyDetails = document.createElement('section');
+      storyDetails.setAttribute('id', 'sd-' + details.id);
+      storyDetails.classList.add('story-details');
+      storyDetails.innerHTML = html;
+
+      var closeButton = storyDetails.querySelector('.js-close');
+      closeButton.addEventListener('click', hideStory.bind(this, details.id));
+
+      document.body.appendChild(storyDetails);
+    }
+
+    // Wait a little time then show the story details.
+    setTimeout(showStory.bind(this, details.id), 30);
+  }
+
+  function showStory(id) {
+
+    var storyDetails = $('#sd-' + id);
+    var left = null;
+
+    function animate () {
+      // Find out where it currently is.
+      var storyDetailsPosition = storyDetails.getBoundingClientRect();
+
+      // Set the left value if we don't have one already.
+      if (left === null)
+        left = storyDetailsPosition.left;
+
+      // Now figure out where it needs to go.
+      left += (0 - storyDetailsPosition.left) * 0.1;
+
+      // Set up the next bit of the animation if there is more to do.
+      if (Math.abs(left) > 0.5)
+        setTimeout(animate, 4);
+      else
+        left = 0;
+
+      // And update the styles. Wait, is this a read-write cycle?
+      // I hope I don't trigger a forced synchronous layout!
+      storyDetails.style.left = left + 'px';
+    }
+
+    // We want slick, right, so let's do a setTimeout
+    // every few milliseconds. That's going to keep
+    // it all tight. Or maybe we're doing visual changes
+    // and they should be in a requestAnimationFrame
+    setTimeout(animate, 4);
+  }
+
+  function hideStory(id) {
+
+    var storyDetails = $('#sd-' + id);
+    var left = 0;
+
+    function animate () {
+      // Find out where it currently is.
+      var mainPosition = main.getBoundingClientRect();
+      var storyDetailsPosition = storyDetails.getBoundingClientRect();
+
+      // Now figure out where it needs to go.
+      left += (mainPosition.width - storyDetailsPosition.left) * 0.1;
+
+      // Set up the next bit of the animation if there is more to do.
+      if (Math.abs(left) > 0.5)
+        setTimeout(animate, 4);
+      else
+        left = 0;
+
+      // And update the styles. Wait, is this a read-write cycle?
+      // I hope I don't trigger a forced synchronous layout!
+      storyDetails.style.left = left + 'px';
+    }
+
+    // We want slick, right, so let's do a setTimeout
+    // every few milliseconds. That's going to keep
+    // it all tight. Or maybe we're doing visual changes
+    // and they should be in a requestAnimationFrame
+    setTimeout(animate, 4);
+  }
+
+  main.addEventListener('scroll', function() {
+    if (main.scrollTop > 0)
+      $('header').classList.add('raised');
+    else
+      $('header').classList.remove('raised');
+  });
+
+  // Bootstrap in the stories.
+  APP.Data.getTopStories(function(data) {
+
+    stories = data;
+
+    for (var i = storyStart; i < count; i++) {
+      if (!stories[i])
+        return;
+
+      var key = String(stories[i]);
+      createStoryListItem(key);
+      APP.Data.getStoryById(stories[i], onStoryData.bind(this, key));
+    }
+
+  });
+
+})();
---
+++ scripts/data.js
@@ -0,0 +1,35 @@
+APP.Data = (function() {
+
+  var HN_API_BASE = 'https://hacker-news.firebaseio.com';
+  var HN_TOPSTORIES_URL = HN_API_BASE + '/v0/topstories.json';
+  var HN_STORYDETAILS_URL = HN_API_BASE + '/v0/item/[ID].json';
+
+  function getTopStories(callback) {
+    request(HN_TOPSTORIES_URL, function(evt) {
+      callback(evt.target.response);
+    });
+  }
+
+  function getStoryById(id, callback) {
+
+    var storyURL = HN_STORYDETAILS_URL.replace(/\[ID\]/, id);
+
+    request(storyURL, function(evt) {
+      callback(evt.target.response);
+    });
+  }
+
+  function request(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = callback;
+    xhr.send();
+  }
+
+  return {
+    getTopStories: getTopStories,
+    getStoryById: getStoryById
+  };
+
+})();
