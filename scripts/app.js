c65cdeaa58aee88d1dde135f81922366f7a186aa
Paul Lewis <github@aerotwist.com>
2015-04-01 20:19:00 +0100

    Adds gitignore; finishes styling work.

--- scripts/app.js
+++ scripts/app.js
@@ -9,15 +9,35 @@ APP.Main = (function() {
   var main = $('main');
   var inDetails = false;
   var storyLoadCount = 0;
+  var localeData = {
+    data: {
+      intl: {
+        locales: 'en-US'
+      }
+    }
+  };
 
-  HandlebarsIntl.registerWith(Handlebars);
+  var tmplStory = $('#tmpl-story').textContent;
+  var tmplStoryDetails = $('#tmpl-story-details').textContent;
+  var tmplStoryDetailsComment = $('#tmpl-story-details-comment').textContent;
+
+  if (typeof HandlebarsIntl !== 'undefined') {
+    HandlebarsIntl.registerWith(Handlebars);
+  } else {
+
+    // Remove references to formatRelative, because Intl isn't supported.
+    var intlRelative = /, {{ formatRelative time }}/;
+    tmplStory = tmplStory.replace(intlRelative, '');
+    tmplStoryDetails = tmplStoryDetails.replace(intlRelative, '');
+    tmplStoryDetailsComment = tmplStoryDetailsComment.replace(intlRelative, '');
+  }
 
   var storyTemplate =
-      Handlebars.compile($('#tmpl-story').textContent);
+      Handlebars.compile(tmplStory);
   var storyDetailsTemplate =
-      Handlebars.compile($('#tmpl-story-details').textContent);
+      Handlebars.compile(tmplStoryDetails);
   var storyDetailsCommentTemplate =
-      Handlebars.compile($('#tmpl-story-details-comment').textContent);
+      Handlebars.compile(tmplStoryDetailsComment);
 
   /**
    * As every single story arrives in shove its
@@ -58,7 +78,7 @@ APP.Main = (function() {
     var storyDetails = $('sd-' + details.id);
 
     // Wait a little time then show the story details.
-    setTimeout(showStory.bind(this, details.id), 30);
+    setTimeout(showStory.bind(this, details.id), 60);
 
     // Create and append the story. A visual change...
     // perhaps that should be in a requestAnimationFrame?
@@ -67,23 +87,26 @@ APP.Main = (function() {
     // it inflates the DOM and I can only see one at once.
     if (!storyDetails) {
 
-      details.urlobj = new URL(details.url);
+      if (details.url)
+        details.urlobj = new URL(details.url);
 
       var comment;
       var commentsElement;
       var storyHeader;
       var storyContent;
 
-      var html = storyDetailsTemplate(details);
+      var storyDetailsHtml = storyDetailsTemplate(details);
       var kids = details.kids;
       var commentHtml = storyDetailsCommentTemplate({
-        by: '', text: ''
+        by: '', text: 'Loading comment...'
       });
 
       storyDetails = document.createElement('section');
       storyDetails.setAttribute('id', 'sd-' + details.id);
       storyDetails.classList.add('story-details');
-      storyDetails.innerHTML = html;
+      storyDetails.innerHTML = storyDetailsHtml;
+
+      document.body.appendChild(storyDetails);
 
       commentsElement = storyDetails.querySelector('.js-comments');
       storyHeader = storyDetails.querySelector('.js-header');
@@ -92,7 +115,6 @@ APP.Main = (function() {
       var closeButton = storyDetails.querySelector('.js-close');
       closeButton.addEventListener('click', hideStory.bind(this, details.id));
 
-      document.body.appendChild(storyDetails);
       var headerHeight = storyHeader.getBoundingClientRect().height;
       storyContent.style.paddingTop = headerHeight + 'px';
 
@@ -112,8 +134,11 @@ APP.Main = (function() {
 
           commentDetails.time *= 1000;
 
-          var comment = commentsElement.querySelector('#sdc-' + commentDetails.id);
-          comment.innerHTML = storyDetailsCommentTemplate(commentDetails);
+          var comment = commentsElement.querySelector(
+              '#sdc-' + commentDetails.id);
+          comment.innerHTML = storyDetailsCommentTemplate(
+              commentDetails,
+              localeData);
         });
       }
     }
@@ -129,6 +154,9 @@ APP.Main = (function() {
 
     var storyDetails = $('#sd-' + id);
     var left = null;
+
+    if (!storyDetails)
+      return;
 
     document.body.classList.add('details-active');
     storyDetails.style.opacity = 1;
@@ -256,12 +284,13 @@ APP.Main = (function() {
     var header = $('header');
     var headerTitles = header.querySelector('.header__title-wrapper');
     var scrollTopCapped = Math.min(70, main.scrollTop);
+    var scaleString = 'scale(' + (1 - (scrollTopCapped / 300)) + ')';
 
     colorizeAndScaleStories();
 
     header.style.height = (156 - scrollTopCapped) + 'px';
-    headerTitles.style.transform = 'scale(' +
-      (1 - (scrollTopCapped / 300)) + ')';
+    headerTitles.style.webkitTransform = scaleString;
+    headerTitles.style.transform = scaleString;
 
     // Add a shadow to the header.
     if (main.scrollTop > 70)
@@ -312,6 +341,7 @@ APP.Main = (function() {
   APP.Data.getTopStories(function(data) {
     stories = data;
     loadStoryBatch();
+    main.classList.remove('loading');
   });
 
 })();
